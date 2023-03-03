8dd71a687b0f1acebc5d44b3fd65794ac9babc1b
Pankaj <pankajsonake27@gmail.com>
2023-03-03 23:27:12 +0530

    array questions

---
+++ Arrays(with searching & sorting)/array questions.cpp
@@ -0,0 +1,56 @@
+#include<iostream>
+using namespace std;
+
+//1)Peak Element(Given an array arr[] of integers. Find a peak element i.e. an element that is not smaller than its neighbors).Only answer function
+int peakElement(int arr[], int n)
+{
+    int s=0;
+    int e=n-1;
+    while(s<=e){
+       int mid=(s+e)/2;
+       if((mid==0 || arr[mid]>=arr[mid-1]) && (mid==n-1 || arr[mid]>=arr[mid+1]) ){
+           return mid;
+       }
+       else if(arr[mid]<=arr[mid+1]){
+           s=mid+1;
+       }
+       else{
+           e=mid-1;
+       }
+   }
+    return -1;
+}
+
+
+
+
+//2)Kth smallest element(Given an array and a number K where K is smaller than the size of the array. Find the K’th smallest element in the given array. Given that all array elements are distinct.)
+ int kthSmallest(int arr[], int l, int r, int k) {
+
+        priority_queue<int> mh;
+        int n=r-l+1;
+        for(int i=0;i<k;i++){
+            mh.push(arr[i]);
+        }
+        for(int i=k;i<n;i++){
+         if(arr[i]<mh.top()){
+             mh.pop();
+                mh.push(arr[i]);
+            }
+        }
+    
+         return mh.top();
+    
+    
+ }
+
+ //3)Given a vector of N positive integers and an integer X. The task is to find the frequency of X in vector.
+ int findFrequency(vector<int> v, int x){
+        int count=0;
+        for(int i=0;i<v.size();i++){
+         if(v[i]==x){
+            count++; 
+         }
+        }
+        return count;
+}
