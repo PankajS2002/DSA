2cbbc54ab9910f9e102a7a668ce00f17e9a7617b
Pankaj <pankajsonake27@gmail.com>
2023-03-08 22:53:27 +0530

    array questions

--- Arrays(with searching & sorting)/array questions.cpp
+++ Arrays(with searching & sorting)/array questions.cpp
@@ -156,3 +156,35 @@ int peakElement(int arr[], int n)
        
        return map.size();
     }
+
+
+//8)Count the triplet
+int countTriplet(int arr[], int n)
+	
+	{  
+	    sort(arr,arr+n);
+	    
+        int count=0;
+        
+        for(int i=n-1;i>=2;i--){
+        int s=0;
+        int e=i-1;
+          while(s<e){    
+            if((arr[s]+arr[e])==arr[i]){
+                count++;
+                s++;
+                e--;
+                
+            }
+            else if((arr[s]+arr[e])<arr[i]){
+                s++;
+            }
+            else{
+                e--;
+            }
+              
+          }
+            
+        }
+        return count;
+	}
