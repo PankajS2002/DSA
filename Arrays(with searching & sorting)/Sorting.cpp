04c6d1dd00e614d5aa2b1051127c5c8d30838f6f
Pankaj <pankajsonake27@gmail.com>
2023-03-11 17:10:19 +0530

    quick sort

--- Arrays(with searching & sorting)/Sorting.cpp
+++ Arrays(with searching & sorting)/Sorting.cpp
@@ -1,6 +1,41 @@
-#include<iostream>
-
+#include<bits/stdc++.h>
 using namespace std;
+/*
+ int partition (int arr[], int low, int high)
+    {
+       int i=low;
+       int j=high;
+       int pivot=arr[low];
+       
+       while(i<j){
+           
+           while(arr[i]<=pivot && i<=high-1){
+               i++;
+           }
+           while(arr[j]>pivot && j>=low+1){
+               j--;
+           }
+          if(i<j){ 
+           swap(arr[i],arr[j]);
+}       }
+       swap(arr[low],arr[j]);
+       return j;
+    }
+
+
+ void quickSort(int arr[], int low, int high)
+    {
+      if(low<high){
+        int part=partition(arr,low,high);
+        quickSort(arr,low,part-1);
+        quickSort(arr,part+1,high);
+      }
+    }
+    
+  */
+   
+
+
 
 int main(){
 
@@ -59,8 +94,11 @@ int main(){
     arr[j+1]=current;
     
   }
-*/ 
 
+
+      quickSort(arr,0,n-1); */ 
+
+    
 
 
 }
