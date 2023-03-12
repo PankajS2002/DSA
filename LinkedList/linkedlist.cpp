a3a7a8ac78031eceaa392441182860b7339d13d0
Pankaj <pankajsonake27@gmail.com>
2023-03-12 20:22:29 +0530

    LinkedList

--- LinkedList/linkedlist.cpp
+++ LinkedList/linkedlist.cpp
@@ -1,20 +1,83 @@
 #include<iostream>
 using namespace std;
 
-class Node{
+class Node
+{
     public:
-       int data;
-       Node* next;
-       Node(int data){
+     int data;
+     Node* next;
+
+     Node(int data)
+     {
         this->data=data;
         this->next=NULL;
-       }
+     }
 };
 
+void insertathead(Node* &head,int data){
+    Node* temp=new Node(data);
+    temp->next=head;
+    head=temp;
+}
+void insertattail(Node* tail,int data){
+    Node* temp=new Node(data);
+    tail->next=temp;
+    tail=temp;
+}
+void insertatany(Node* head,int data,int position){
+      Node* temp=head;
+      int cnt=1;
+      while(cnt!=position-1){
+        temp=temp->next;
+        cnt++;
+      }
 
+      Node* nodetoinsert=new Node(data);
+      nodetoinsert->next=temp->next;
+      temp->next=nodetoinsert;
+
+}
+void deletenode(Node* &head,int position){
+      if(position==1){
+        Node* temp=head;
+         head=head->next;
+         temp->next=NULL;
+         delete temp;
+      }
+      else{
+        Node* curr=head;
+        Node* prev=NULL;
+        int cnt=1;
+        while(cnt<position){
+            prev=curr;
+            curr=curr->next;
+            cnt++;
+        }
+        prev->next=curr->next;
+        curr->next=NULL;
+        delete curr;
+      }
+}
+
+void print(Node* &head){
+    Node* temp=head;
+    while (temp!=NULL)
+    {
+        cout<<temp->data<<" ";
+         temp=temp->next;
+    }
+    
+}
 int main(){
     Node* node1=new Node(10);
-    cout<<node1->data;
-    
+    Node* head=node1;
+    Node* tail=node1;
+    insertathead(head,30);
+    insertathead(head,40);
+    insertattail(tail,5);
+    insertatany(head,35,2);
+    deletenode(head,1);
+    print(head);
+   
 
 }
