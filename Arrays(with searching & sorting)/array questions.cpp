ec329a4a9f58123d176c7c0531a6ff779ac4d364
Pankaj <pankajsonake27@gmail.com>
2023-03-21 22:02:46 +0530

    array questions

--- Arrays(with searching & sorting)/array questions.cpp
+++ Arrays(with searching & sorting)/array questions.cpp
@@ -342,3 +342,24 @@ long long maxSubarraySum(int arr[], int n){
             }
         
         } 
+
+//18) Merge intervals
+     vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        vector<vector<int>> mergedIntervals;
+        if(intervals.size() == 0) {
+            return mergedIntervals; 
+        }
+        sort(intervals.begin(), intervals.end());
+        vector<int> tempInterval = intervals[0]; 
+        
+        for(auto it : intervals) {
+            if(it[0] <= tempInterval[1]) {
+                tempInterval[1] = max(it[1], tempInterval[1]); 
+            } else {
+                mergedIntervals.push_back(tempInterval); 
+                tempInterval = it; 
+            }
+        }
+        mergedIntervals.push_back(tempInterval);
+        return mergedIntervals; 
+    }
