5df271f2e6d1834b64eb83dbef6c418bed12d4ab
Pankaj <pankajsonake27@gmail.com>
2023-04-21 22:30:36 +0530

    array questions

--- Arrays(with searching & sorting)/array questions.cpp
+++ Arrays(with searching & sorting)/array questions.cpp
@@ -1049,4 +1049,148 @@ double MedianOfArrays(vector<int>& array1, vector<int>& array2)
        return v[(v.size()-1)/2];   
      }
 
-     //42)
+     //42)kth smallest element in the matrix
+     
+  int kthSmallest(vector<vector<int>>& matrix, int k) {
+        
+        int n = matrix.size();
+        int low = matrix[0][0];
+        int high = matrix[n-1][n-1];
+        while(low < high){
+            
+            int mid = low + (high - low)/2;
+            int count = 0;
+            for(int i = 0; i < n; i++){
+                count += upper_bound(matrix[i].begin(), matrix[i].end(), mid) - matrix[i].begin();
+                
+            }
+            
+            if(count < k){
+                low = mid + 1;
+            }else{
+                high = mid;
+            }
+            
+        }
+        
+        return low;
+    }
+
+     //43)find row with maximum no. of 1
+     int rowWithMax1s(vector<vector<int> > arr, int n, int m) {
+	int row=-1;
+	int i=0;
+	int j=m-1;
+	while(i<n && j>=0){
+	    if(arr[i][j]==1){
+	        j--;
+	        row=i;
+	    }
+	    else{
+	        i++;
+	    }
+	}
+	return row;
+	
+}
+
+  //44)sort matrix
+  vector<vector<int>> sortedMatrix(int N, vector<vector<int>> Mat) {
+       vector<int> nig;
+       for(int i=0;i<N;i++){
+           for(int j=0;j<N;j++){
+               nig.push_back(Mat[i][j]);
+           }
+       }
+       
+       sort(nig.begin(),nig.end());
+       int q=0;
+       for(int i=0;i<N;i++){
+           for(int j=0;j<N;j++){
+              Mat[i][j]=nig[q];
+              q++;
+           }
+       }
+       
+       return Mat;
+       
+    }
+
+    //45)Max rectangle
+    int maxHist(int row[],int C)
+{
+    // Create an empty stack.
+    // The stack holds indexes of
+    // hist[] array/ The bars stored
+    // in stack are always
+    // in increasing order of their heights.
+    stack<int> result;
+ 
+    int top_val; // Top of stack
+ 
+    int max_area = 0; // Initialize max area in current
+    // row (or histogram)
+ 
+    int area = 0; // Initialize area with current top
+ 
+    // Run through all bars of given histogram (or row)
+    int i = 0;
+    while (i < C) {
+        // If this bar is higher than the bar on top stack,
+        // push it to stack
+        if (result.empty() || row[result.top()] <= row[i])
+            result.push(i++);
+ 
+        else {
+            // If this bar is lower than top of stack, then
+            // calculate area of rectangle with stack top as
+            // the smallest (or minimum height) bar. 'i' is
+            // 'right index' for the top and element before
+            // top in stack is 'left index'
+            top_val = row[result.top()];
+            result.pop();
+            area = top_val * i;
+ 
+            if (!result.empty())
+                area = top_val * (i - result.top() - 1);
+            max_area = max(area, max_area);
+        }
+    }
+ 
+    // Now pop the remaining bars from stack and calculate
+    // area with every popped bar as the smallest bar
+    while (!result.empty()) {
+        top_val = row[result.top()];
+        result.pop();
+        area = top_val * i;
+        if (!result.empty())
+            area = top_val * (i - result.top() - 1);
+ 
+        max_area = max(area, max_area);
+    }
+    return max_area;
+}
+ 
+    int maxArea(int M[MAX][MAX], int n, int m) {
+      
+
+    // Calculate area for first row and initialize it as
+    // result
+    int result = maxHist(M[0],m);
+ 
+    for (int i = 1; i < n; i++) {
+ 
+        for (int j = 0; j < m; j++)
+ 
+            // if A[i][j] is 1 then add A[i -1][j]
+            if (M[i][j])
+                M[i][j] += M[i - 1][j];
+ 
+        // Update result if area with current row (as last
+        // row) of rectangle) is more
+        result = max(result, maxHist(M[i],m));
+    }
+ 
+    return result;
+}
+    
