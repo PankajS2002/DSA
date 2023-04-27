b45f9c75847a41426930e5970d10131f39d87daf
Pankaj <pankajsonake27@gmail.com>
2023-04-27 22:12:52 +0530

    string questions

--- Strings/stringquestions.cpp
+++ Strings/stringquestions.cpp
@@ -175,7 +175,7 @@ bool isInterleave(string A, string B, string C)
 	return t[n][n];
 		}
 
-        //8)longest palindrom string
+        //8)longest palindrome string
          string longestPalin (string S) {
         string result="";
         int start=0;
@@ -214,4 +214,87 @@ bool isInterleave(string A, string B, string C)
             result=result+S[i];
         }
         return result;
+    }
+
+    //9)longest common subsequence
+    int lcs(int x, int y, string s1, string s2)
+    {
+     
+		  int dp[x+1][y+1];
+		  
+		  for(int i=0;i<=x;i++){
+		      for(int j=0;j<=y;j++){
+		          if(i==0 or j==0){
+		              dp[i][j]=0;
+		          }
+		      }
+		  }
+		  
+		  for(int i=1;i<=x;i++){
+		      for(int j=1;j<=y;j++){
+		          if(s1[i-1]==s2[j-1]){
+		              dp[i][j]=1+dp[i-1][j-1];
+		          }
+		          else{
+		              dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+		          }
+		      }
+		  }
+		  
+		  return dp[x][y];
+    }
+
+    //10)longest repeating subsequence
+    int LongestRepeatingSubsequence(string str){
+		  int n=str.length();
+		  int dp[n+1][n+1];
+		  
+		  for(int i=0;i<=n;i++){
+		      for(int j=0;j<=n;j++){
+		          if(i==0 or j==0){
+		              dp[i][j]=0;
+		          }
+		      }
+		  }
+		  
+		  for(int i=1;i<=n;i++){
+		      for(int j=1;j<=n;j++){
+		          if(str[i-1]==str[j-1] && i!=j){
+		              dp[i][j]=1+dp[i-1][j-1];
+		          }
+		          else{
+		              dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+		          }
+		      }
+		  }
+		  
+		  return dp[n][n];
+		
+		    
+		}
+
+        //11)string subsequence game
+         void subseq(string s,string temp,unordered_set<string> &st){
+        if(s.empty()){
+            st.insert(temp);
+            return;
+        }
+        subseq(s.substr(1),temp+s[0],st);
+        subseq(s.substr(1),temp,st);
+    }
+    set<string> allPossibleSubsequences(string S) {
+        unordered_set<char> s;
+        s.insert('a');
+        s.insert('e');
+        s.insert('i');
+        s.insert('o');
+        s.insert('u');
+        unordered_set<string> st;
+        subseq(S,"",st);
+        set<string> res;
+        for(auto subs:st){
+            // cout<<subs<<" ";
+            if(s.find(subs[0])!=s.end() and s.find(subs.back())==s.end()) res.insert(subs);
+        }
+        return res;
     }
